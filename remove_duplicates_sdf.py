4ede8e593c2b8a351b4dc51c461f4cf6e5a2e11d
pkyawzi <pkyawzi@ncsu.edu>
2019-11-14 17:35:43 -0500

    comments added

--- remove_duplicates_sdf.py
+++ remove_duplicates_sdf.py
@@ -16,23 +16,24 @@ from tkinter.filedialog import askopenfilename
 import os
 
 def main():
+
     # Choose the sdf file when prompted
-
     filename = askopenfilename()
-    df = PandasTools.LoadSDF(filename, molColName='structure')
-    num_original = df.shape[0]
-    suppl = Chem.SDMolSupplier(filename)
-    mols = [x for x in suppl]
-    inchis = [Chem.inchi.MolToInchi(x) for x in mols]
-    df['inchis'] = inchis
+    df = PandasTools.LoadSDF(filename, molColName='structure')  # Load sdf file to pandas
+    num_original = df.shape[0]                             # get number of original compounds
 
-    df = df.drop_duplicates(subset=['inchis'])
-    num_current = df.shape[0]
-    num_removed = num_original - num_current
+    suppl = Chem.SDMolSupplier(filename)                   # load sdf file with rdkit
+    mols = [x for x in suppl]                              # get mols from sdf file
+    inchis = [Chem.inchi.MolToInchi(x) for x in mols]      # generate inchi keys from mols
+    df['inchis'] = inchis                                  # add "inchis" column to pandas frame
+
+    df = df.drop_duplicates(subset=['inchis'])             # drop duplicate rows based on "inchis" column
+    num_current = df.shape[0]                              # check number of compounds after duplicate removal
+    num_removed = num_original - num_current               # check how many were removed
     print(str(num_removed) + ' duplicate compounds were removed.')
 
-    df = df.loc[:, df.columns != 'inchis']
-    PandasTools.WriteSDF(df,filename[:-4]+'_DuplicatesRemoved.sdf',molColName='structure',properties=list(df.columns))
+    df = df.loc[:, df.columns != 'inchis']                 # remove the temporarily added column of "inchis"
+    PandasTools.WriteSDF(df,filename[:-4]+'_DuplicatesRemoved.sdf',molColName='structure',properties=list(df.columns))  # export _DuplicatesRemoved sdf file
 
 if __name__ == '__main__':
     main()
